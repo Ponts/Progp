b58acb41c5cdeeef9547b2b81c53b10cd01bbe3d
AggeFalk <augfal@kth.se>
2015-09-18 12:03:54 +0200

    removed tab characters

--- F2.hs
+++ F2.hs
@@ -5,17 +5,17 @@ module F2 where
 import Data.List
 -- Datatyper
 data MolSeq = MolSeq {seqName :: String, seqSequence :: String, seqType :: String}
-data Profile= Profile {matrix :: [[(Char, Int)]], nrOfSequences :: Int, name :: String} 
+data Profile= Profile {matrix :: [[(Char, Int)]], profSeqType :: String, nrOfSequences :: Int, name :: String} 
 
 -- Tar två strängar och avgör om det är en protein eller dna
 -- Returnar MolSeq protein eller dna
 string2seq :: String -> String -> MolSeq
 string2seq namn sekvens = if isProtein sekvens
-				then MolSeq namn sekvens "Protein" -- är Protein
-				else MolSeq namn sekvens "DNA"
+                then MolSeq namn sekvens "Protein" -- är Protein
+                else MolSeq namn sekvens "DNA"
 
 molseqs2profile :: String -> [MolSeq] -> Profile
-molseqs2profile string mols = Profile (makeProfileMatrix mols) (length mols) string
+molseqs2profile name mols = Profile (makeProfileMatrix mols) (seqType (head mols)) (length mols) name
 
 
 nucleotides = "ACGT"
@@ -34,17 +34,17 @@ makeProfileMatrix sl = res
     strs = map seqSequence sl                              -- Rad (iii) strs är en lista som håller alla sekvenserna
     tmp1 = map (map (\x -> ((head x), (length x))) . group . sort)
                (transpose strs)                            -- Rad (iv) transpose strs är gör om strängarna i strs så att den första strängen i listan är den första bokstaven i varje sträng från strs
-               											   -- första map gör det inom första parentesen på transpose strs
-               											   -- head x tar första 
+                                                           -- första map gör det inom första parentesen på transpose strs
+                                                           -- head x tar första 
     equalFst a b = (fst a) == (fst b)
     res = map sort (map (\l -> unionBy equalFst l defaults) tmp1)
 
 -- Kollar om det är en DNA.
 isProtein :: String -> Bool
-isProtein [] = False 				-- Kommer vi hit är det en Protein
+isProtein [] = False                -- Kommer vi hit är det en Protein
 isProtein (c : rest) 
-				| elem c proteinChars = True	-- kommer vi hit är det en protein
-				| otherwise = isProtein(rest)	-- Är en char, kolla nästa
+                | elem c proteinChars = True    -- kommer vi hit är det en protein
+                | otherwise = isProtein(rest)   -- Är en char, kolla nästa
 
 -- Chars som finns i Protein
 proteinChars = ['R','N','D','E','Q','H','I','L','K','M','F','P','S','W','Y','V']
@@ -55,23 +55,23 @@ seqLength m = length (seqSequence m)
 
 seqDistance :: MolSeq -> MolSeq -> Double
 seqDistance m1 m2 = case molTypes m1 m2 of 
-						("Protein", "Protein")	-> calcProtein m1 m2
-						("DNA", "DNA")	-> calcDNA m1 m2
-						_				-> error "Invalid types"
-	
-	
+                        ("Protein", "Protein")  -> calcProtein m1 m2
+                        ("DNA", "DNA")  -> calcDNA m1 m2
+                        _               -> error "Invalid types"
+    
+    
 calcDNA :: MolSeq -> MolSeq -> Double
 calcDNA m1 m2 = if hammingDist m1 m2 > 0.74
-			then 3.3
-			else -(3/4)*log(1-(4*hammingDist m1 m2)/3)
-		
+            then 3.3
+            else -(3/4)*log(1-(4*hammingDist m1 m2)/3)
+            
 calcProtein :: MolSeq -> MolSeq -> Double
 calcProtein m1 m2 = if hammingDist m1 m2 > 0.94
-			then 3.7
-			else -(19/20)*log(1-(20*hammingDist m1 m2)/19)
-				
+            then 3.7
+            else -(19/20)*log(1-(20*hammingDist m1 m2)/19)
+        
 -- Tar in två sekvenser
--- Returnerar en Tuple med två Boolean som säger om sekvenserna är DNA eller inte				   
+-- Returnerar en Tuple med två Strings som säger om sekvenserna är DNA eller Protein				   
 molTypes :: MolSeq -> MolSeq -> (String,String)
 molTypes m1 m2 = (seqType m1, seqType m2)
 
@@ -83,6 +83,5 @@ hammingDist m1 m2 = countDiff (seqSequence m1) (seqSequence m2) / fromIntegral (
 countDiff :: String -> String -> Double
 countDiff [] [] = 0.0 -- Strängarna är samma längd så detta funkar 
 countDiff s1 s2
-					| head s1 /= head s2 = 1.0 + countDiff (tail s1) (tail s2)
-					| otherwise = countDiff (tail s1) (tail s2)
-
+                    | head s1 /= head s2 = 1.0 + countDiff (tail s1) (tail s2)
+                    | otherwise = countDiff (tail s1) (tail s2)
